a65d3ed4ba76f980893492d9689fc1c4868c79d2
prakashnanjappa <prakashnanjappa94@gmail.com>
2018-02-09 16:52:26 +0530

    added comments file

---
+++ comments.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int i; //int data type is used to stored the +ve/-ve integers
+ char ch; // char data type is used to store single character
+ float f1; // float dat type is used to store number with decimal values
+ printf("comments topic is used to stored extra information about code");
+ printf("\n");
+ /*ffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffff
+*/
+}
+
