2402284cddc2cbef7e32523b7ae5517499906b00
prakashnanjappa <prakashnanjappa94@gmail.com>
2018-02-10 13:58:19 +0530

    added modify file

---
+++ modify.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//#include<conio.h>
+int main()
+{
+int x,y,z;
+x=10;
+y=12;
+z=34;
+printf("%d \n",x);
+printf("%d \n",x++);
+printf("%d \n",y);
+printf("%d \n" ,y++);
+printf("%d \n",z);
+printf("%d \n",z++);
+return 0;
+}
+
